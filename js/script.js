631f7f32a8bfb5f79c6292e7f34005c5fdabf2ef
shabeerp15 <84959692+shabeerp15@users.noreply.github.com>
2021-07-14 14:07:24 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,77 @@
+let validated ={
+    name:false,
+    email:false,
+    phone:false,
+    message:false
+}
+
+function nameChek() {
+    var nameAlert = document.getElementById('name-alert');
+    var name = document.getElementById("name").value;
+    var expression = /^[a-zA-Z\s]*$/;
+
+    if (name == "") {
+        validated.name = false;
+        nameAlert.innerHTML = "Required"
+
+    } else if (name.match(expression)) {
+        nameAlert.innerHTML = "";
+        validated.name = true;
+    } else {
+        nameAlert.innerHTML = " Enter Charecters only";
+        validated.name = false;
+    }
+
+}
+
+function chekEmail() {
+    var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    var emailCheck = document.getElementById('email').value;
+    var emailAlert = document.getElementById('email-alert');
+    if (emailCheck == "") {
+        emailAlert.innerHTML = "Required";
+        validated.email = false;
+    } else if (emailCheck.match(pattern)) {
+        emailAlert.innerHTML = "";
+        validated.email = true;
+    } else {
+        emailAlert.innerHTML = "Invalid E-mail"
+        validated.email = false
+    }
+}
+
+function checkNumber() {
+    var phnPattern = /^\d{10}$/;
+    var phoneNumber = document.getElementById('mobile').value;
+    var phoneAlert = document.getElementById('mobile-alert');
+    if (phoneNumber == "") {
+        validated.phone = false;
+        phoneAlert.innerHTML = "Required";
+    } else if (phoneNumber.match(phnPattern)) {
+        phoneAlert.innerHTML = "";
+        validated.phone = true;
+    } else {
+        phoneAlert.innerHTML = "Invalid Mobile Number";
+        validated.phone = false
+    }
+}
+
+function checkMessage() {
+    var messageValue = document.getElementById("message").value;
+    if (messageValue.length <= 0) {
+        document.getElementById("message-alert").innerHTML = "Enter Your Message"
+        validated.message = false;
+    }
+    else if (messageValue.length <= 15) {
+        document.getElementById("message-alert").innerHTML = "Enter atleast 15 charecter";
+        validated.message = false;
+    }
+    else {
+        document.getElementById("message-alert").innerHTML = "";
+        validated.message = true;
+    }
+}
+
+
+
+
